9fecb2e1b60eab9486283536a46c62fd16805a01
Harry <rlatjsdn1261@naver.com>
2019-11-06 23:03:18 +0900

    Init project & pods

---
+++ tingting/tingting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tingting
+//
+//  Created by 김선우 on 11/6/19.
+//  Copyright © 2019 Harry Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
