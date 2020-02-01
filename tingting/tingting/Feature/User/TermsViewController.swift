ec53b7681b7c19ee786458c2f96f36bfa001c85a
Harry Kim <rlatjsdn1261@naver.com>
2020-02-02 04:30:35 +0900

    Init TermsVC and Add M13Checkbox

---
+++ tingting/tingting/Feature/User/TermsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TermsViewController.swift
+//  tingting
+//
+//  Created by 김선우 on 2/2/20.
+//  Copyright © 2020 Harry Kim. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TermsViewController: UITableViewController {
+
+}
+
+extension TermsViewController {
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+}
