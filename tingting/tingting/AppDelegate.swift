10dc70435e3943cd903dd9b522a8ba19fdd7a708
Harry Kim <rlatjsdn1261@naver.com>
2020-02-24 23:59:14 +0900

    Connect Firebase/Analytics

--- tingting/tingting/AppDelegate.swift
+++ tingting/tingting/AppDelegate.swift
@@ -10,14 +10,23 @@ import UIKit
 import CoreData
 import SwiftyBeaver
 import Kingfisher
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Logger
         Logger.addDestination(ConsoleDestination())
+        
+        // Kingfisher 메모리 초기화
         KingfisherManager.shared.cache.clearMemoryCache()
         KingfisherManager.shared.cache.clearDiskCache()
+        
+        // Firebase
+        FirebaseApp.configure()
+        
         return true
     }
 
