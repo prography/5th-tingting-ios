129ed4e59d1cf8103b38e3bde5f9d27773176480
Harry Kim <rlatjsdn1261@naver.com>
2020-03-08 22:23:18 +0900

    Add test - login

--- tingting/tingtingTests/tingtingTests.swift
+++ tingting/tingtingTests/tingtingTests.swift
@@ -18,17 +18,52 @@ class tingtingTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+ 
+    func test_login() {
+        // given
+        let requset = APIModel.Login.Request(id: "sunwoo", password: "123456")
+        let expect = expectation(description: "로그인이 성공해야함.")
+              
+        // when
+        _ = NetworkManager.login(request: requset)
+            .asObservable()
+            .subscribe(
+                onNext: { response in
+                    // then
+                    XCTAssert(response.message.count > 0)
+                    XCTAssert(response.token.count > 0)
+                    expect.fulfill()
+            }, onError: { error in
+                XCTAssert(false)
+                expect.fulfill()
+            })
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_wrongLogin() {
+        
+        // given
+        let requset = APIModel.Login.Request(id: "wrongID", password: "wrongPassword")
+        let expect = expectation(description: "error를 타아햐는 케이스")
+              
+        // when
+        _ = NetworkManager.login(request: requset)
+            .asObservable()
+            .subscribe(
+                onNext: { response in
+                    XCTAssert(false)
+            }, onError: { _ in
+                XCTAssert(true)
+                expect.fulfill()
+            })
+        waitForExpectations(timeout: 1, handler: nil)
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
     }
 
 }
